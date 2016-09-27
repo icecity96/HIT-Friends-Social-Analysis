c3cd6322598565123219e0e9126e6646b86c020b
ice_city <ice_city@172.20.21.216>
2016-09-27 20:02:26 +0800

    改变文件结构

--- HFSA/src/main/java/com/service/impl/PassWord.java
+++ HFSA/src/main/java/com/service/impl/PassWord.java
@@ -1,4 +1,4 @@
-package com.service;
+package com.service.impl;
 
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
@@ -36,22 +36,20 @@ public class PassWord {
 	 * 合法密码应当至少8个字符，至少包含一个数字，至少包含一个小写字母和一个大写字母
 	 * 特殊字符至少出现一个，
 	 * 不包含空格，制表符
+	 * explain:
+	 * ^                 # start-of-string
+	 * (?=.*[0-9])       # a digit must occur at least once
+	 * (?=.*[a-z])       # a lower case letter must occur at least once
+	 * (?=.*[A-Z])       # an upper case letter must occur at least once
+	 * (?=.*[@#$%^&+=])  # a special character must occur at least once
+	 * (?=\\S+$)          # no whitespace allowed in the entire string
+	 * .{8,}             # anything, at least eight places though
+	 * $                 # end-of-string
 	 * @param plaintext
 	 * @return 密码格式是否合法
 	 */
 	public static boolean isLegitimate(String plaintext) {
-		/**
-		 * explain:
-		 * ^                 # start-of-string
-		 * (?=.*[0-9])       # a digit must occur at least once
-		 * (?=.*[a-z])       # a lower case letter must occur at least once
-		 * (?=.*[A-Z])       # an upper case letter must occur at least once
-		 * (?=.*[@#$%^&+=])  # a special character must occur at least once
-		 * (?=\S+$)          # no whitespace allowed in the entire string
-		 * .{8,}             # anything, at least eight places though
-		 * $                 # end-of-string
-		 */
-		String pattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[$&+,:;=?@#|'<>.-^*()%!])(?=\\S+$).{8,}$";
+		String pattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[$&+,:;=?@#|'<>.^*()%!-])(?=\\S+$).{8,}$";
 		return plaintext.matches(pattern);
 	}
 }
