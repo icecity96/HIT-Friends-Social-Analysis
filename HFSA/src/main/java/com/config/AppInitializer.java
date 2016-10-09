ecb2091bcced4ca6092c32cb566824005b9e4a66
ice_city <ice_city@172.20.73.31>
2016-10-09 16:58:37 +0800

    纯注解的方式整合spring－mybatis

---
+++ HFSA/src/main/java/com/config/AppInitializer.java
@@ -0,0 +1,36 @@
+package com.config;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+
+/**
+ * this class will replace web.xml
+ * @author ice_city
+ *
+ */
+public class AppInitializer implements WebApplicationInitializer{
+
+	@Override
+	public void onStartup(ServletContext container) throws ServletException {
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+		ctx.register(SpringConfig.class);
+		ctx.setServletContext(container);
+		
+		ServletRegistration.Dynamic servlet = container.addServlet("dispatcher", new DispatcherServlet(ctx));
+		
+		servlet.setLoadOnStartup(1);
+		servlet.addMapping("/");
+		
+		//编码设置
+		FilterRegistration.Dynamic fr = container.addFilter("encodingFilter", new CharacterEncodingFilter());
+		fr.setInitParameter("encoding", "UTF-8");
+		fr.setInitParameter("forceEncoding", "true");
+	}
+}
---
+++ HFSA/src/main/java/com/config/DataSourceConfig.java
@@ -0,0 +1,35 @@
+package com.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DataSourceConfig {
+	//WHO 用来区分是小组谁在用，以控制数据库信息，请修改时只修改WHO的信息
+	//不要动源码内容
+	private final String WHO = "icecity";
+	@Bean
+	public DataSource getDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		if (WHO.equals("icecity")) {
+			dataSource.setUrl("jdbc:mysql://localhost:3306/HFSA");
+			dataSource.setUsername("root");
+			dataSource.setPassword("lmy19960521");
+		} else if (WHO.equals("lx")) {
+			//lx 修改个人配置区
+			dataSource.setUrl("jdbc:mysql://localhost:3306/HFSA");
+			dataSource.setUsername("root");
+			dataSource.setPassword("lmy19960521");
+		} else {
+			//gaoxy 修改个人配置区域
+			dataSource.setUrl("jdbc:mysql://localhost:3306/HFSA");
+			dataSource.setUsername("root");
+			dataSource.setPassword("lmy19960521");
+		}
+		return dataSource;
+	}
+}
